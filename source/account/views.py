99632daa1ef2638b30b987ab346e936704ca9bfc
Said Saliev <repsaid@gmail.com>
2023-01-14 16:08:25 +0600

    add app account, create login logout and registration

---
+++ source/account/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from account.form import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    form_class = MyUserCreationForm
+    template_name = 'user_create.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+
+        next_url = self.request.POST.get('next')
+        if next_url:
+            return next_url
+
+        return reverse('webapp:product_index')
