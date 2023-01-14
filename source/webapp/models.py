043c7c3e27771ce6044de7fad4dd11c26534c0be
Said Saliev <repsaid@gmail.com>
2023-01-14 13:53:14 +0600

    add default.img

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=30, verbose_name="Заголовок")
     category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, verbose_name="Категория")
     description = models.TextField(max_length=3000, verbose_name="Описание", null=True, blank=True)
-    avatar = models.ImageField(blank=True, null=True, upload_to='product_avatar', verbose_name="Аватар")
+    avatar = models.ImageField(blank=True, null=True, upload_to='product_avatar', verbose_name="Аватар", default='product_avatar/default.jpg')
 
     def get_absolute_url(self):
         return reverse('webapp:product', kwargs={'pk': self.pk})
