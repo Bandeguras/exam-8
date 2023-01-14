a6562f3930329ec93dea1378305dc5eb960c8b2a
Said Saliev <repsaid@gmail.com>
2023-01-14 16:11:55 +0600

    add validation email required

--- source/account/form.py
+++ source/account/form.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import forms
 
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if email is '':
+            raise forms.ValidationError('Поля email обязательно')
+        return email
